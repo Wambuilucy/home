6ce085982f9b969470f19f7ace9ab4fa663d955a
Wambuilucy <wambuilucie99@gmail.com>
2019-11-30 15:27:58 +0300

    removed content and added new content

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
-const maleNames = [
-    let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];	  "Kwasi",
-      "Kwadwo",
-      "Kwabena",
-      "Kwaku",
-      "Yaw",
-      "Kofi",
-      "Kwame"
+function submitForm(event) {
+    event.preventDefault();
+    var date = document.forms["akanForm"]["birthday"].value;
+    var gender = document.forms["akanForm"]["gender"].value;
+    var akanName = "";
+
+    var femaleNames = [
+        "Akosua",
+        "Adwoa",
+        "Abenaa",
+        "Akua",
+        "Yaa",
+        "Afua",
+        "Ama"
     ];
-    const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    
-     function validateInput() {	function validateInput() {
-      var year = document.forms["akanForm"]["year"].value;	  let year = document.forms["akanForm"]["year"].value;
-      var gender = document.forms["akanForm"]["gender"].value;	  let gender = document.forms["akanForm"]["gender"].value;
-    
-    
-       if (year != null && year != undefined && year != "") {	  if (year != null && year != undefined && year != "") {
-        $("#exampleModal").modal("show");	    $("#akanModal").modal("show");
-        document.getElementById("actualAkanName").innerHTML = `${getName(	    document.getElementById("actualAkanName").innerHTML = `${getAkanName(
-          year,	      year,
-          gender	      gender
-        )}`;	    )}`;
-      } else {	  } else {
-        alert("Please enter a valid birth date");	    alert("Please enter a valid birth date");
-      }	  }
-    }	}
-    var getName = (year, gender) => {	var getAkanName = (year, gender) => {
-      let day = new Date(year);	  let day = new Date(year);
-      let dayNumber = day.getDay();	  let dayNumber = day.getDay();
-      let name = "";	  let name = "";
+
+    var maleNames = [
+        "Kwasi",
+        "Kwadwo",
+        "Kwabena",
+        "Kwaku",
+        "Yaw",
+        "Kofi",
+        "Kwame"
+    ];
+
+    var dateObject = new Date(date);
+    var day = dateObject.getDay();
+
+    if (day != null && gender != null) {
+        if (gender === "male") {
+            akanName = maleNames[day];
+        }
+        else {
+            akanName = femaleNames[day];
+        }
     }
-    
+    document.getElementById("akanName").innerHTML = akanName;
+}
+
+
+
+
+
